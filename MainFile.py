69536f6ba76fe0935b64a4ddb3a196c2ceb670bd
AvishrantsSh <avishrants@gmail.com>
2021-02-06 14:16:04 +0530

    Basic UI Created

--- MainFile.py
+++ MainFile.py
@@ -59,4 +59,75 @@ def call_mouse(event, x, y, flag, param):
 
 # Main Code Starts Here
 if __name__ == "__main__":
-    print("ToDo")
+    print("Window Setup")
+    image = get_image()
+    text_box = np.zeros((sizeBlank, 2*size + sizeBlank, 3)) + 1.
+    empty = np.zeros((size, size, 3))
+    blank = np.zeros((size, sizeBlank, 3)) + 1
+
+    namedWindow("Object Removal Window", WINDOW_NORMAL)
+    setMouseCallback('Object Removal Window', call_mouse)
+    
+    # Prerained model path
+    pretrained_model = './Model/pre_model'
+
+    # Tensorflow and Model Init
+    sess = tf.compat.v1.InteractiveSession()
+    isTraining = tf.compat.v1.placeholder(tf.bool)
+    images_placeholder = tf.compat.v1.placeholder(tf.float32, shape=[1, size, size, 3], name="images")
+
+    # Initiliasing our remover
+    model = Reconstruct()
+
+    recon_gen = model.generator(images_placeholder, isTraining)
+    saver = tf.compat.v1.train.Saver(max_to_keep=100)
+    saver.restore(sess, pretrained_model)
+
+    createTrackbar('Pen Size', 'Object Removal Window', 1, 10, lambda x: x)
+    # Widget for pensize
+
+    filtered_image = empty
+    # Filtered image which is initially empty
+
+    while(True):
+        # Windows and text
+        sub_window = np.hstack((image, blank, filtered_image[:, :, [2, 1, 0]]))
+        window = np.vstack((sub_window, text_box))
+        imshow('Object Removal Window', window)
+        putText(text_box, 'Image', (110, 15), font, 0.4, (0, 0, 0), 1)
+        putText(text_box, 'Reconstructed Image', (130 + size, 15), font, 0.4, (0, 0, 0), 1)
+
+        # Key Events
+        key_pressed = waitKey(1) & 0xFF
+
+        # ESC Key
+        if key_pressed == 27:
+            break
+
+        # F Key for filter
+        elif key_pressed == 102:
+            input_masked = masking(image)
+            input_masked = input_masked[:, :, [2, 1, 0]]
+            shape = np.array(input_masked).shape
+            input_tensor = np.array(input_masked).reshape(
+                1, shape[0], shape[1], shape[2])
+            output_tensor = sess.run(
+                recon_gen,
+                feed_dict={
+                    images_placeholder: input_tensor,
+                    isTraining: False
+                }
+            )
+            filtered_image = np.array(output_tensor)[0, :, :, :].astype(float)
+            # imwrite(os.path.join('results', path_images[img_no][21 : 35]), ((filtered_image[:,:,[2, 1, 0]]) * 255) )
+            # imwrite(os.path.join('inputs', path_images[img_no][21 : 35]), ((image) * 255))
+
+        # R key to reset
+        elif key_pressed == 114:
+            image = get_image(this=True)
+            filtered_image = empty
+
+        # Adjust pen size
+        stroke_size = getTrackbarPos('Pen Size', 'Object Removal Window')
+
+    destroyAllWindows()
