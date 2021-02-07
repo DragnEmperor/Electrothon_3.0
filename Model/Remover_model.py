82441b0e3216e8a74a1873747e760f0a3d4e3aa3
DragnEmperor <41839176+DragnEmperor@users.noreply.github.com>
2021-02-07 11:19:00 +0530

    Update Remover_model.py

--- Model/Remover_model.py
+++ Model/Remover_model.py
@@ -2,7 +2,7 @@ import tensorflow as tf
 import numpy as np
 tf.compat.v1.disable_v2_behavior()
 
-# Build a class for model
+# Class Build for the model
 class Reconstruct():
     def __init__(self):
         pass
@@ -36,7 +36,6 @@ class Reconstruct():
         dim = np.prod( shape[1:] )
         x = tf.reshape( bottom, [-1, dim])
         input_size = dim
-
         with tf.compat.v1.variable_scope(name):
             w = tf.compat.v1.get_variable("W", shape=[input_size, output_size], initializer=tf.compat.v1.random_normal_initializer(0., 0.005))
             b = tf.compat.v1.get_variable("b", shape=[output_size], initializer=tf.compat.v1.constant_initializer(0.))
@@ -47,33 +46,10 @@ class Reconstruct():
     def leaky_relu(self,new_input):
         return tf.nn.leaky_relu(new_input,alpha=0.1,)
 
-    def batchnorm(self, bottom, is_train, epsilon=1e-8, name=None):
-        bottom = tf.clip_by_value( bottom, -100., 100.)
-        depth = bottom.get_shape().as_list()[-1]
-
-        with tf.compat.v1.variable_scope(name):
-            gamma = tf.compat.v1.get_variable("gamma", [depth], initializer=tf.compat.v1.constant_initializer(1.))
-            beta  = tf.compat.v1.get_variable("beta" , [depth], initializer=tf.compat.v1.constant_initializer(0.))
-
-            batch_mean, batch_var = tf.nn.moments(x=bottom, axes=[0,1,2], name='moments')
-            ema = tf.train.ExponentialMovingAverage(decay=0.5)
-
-            def update():
-                with tf.control_dependencies([ema_apply_op]):
-                    return tf.identity(batch_mean), tf.identity(batch_var)
-
-            ema_apply_op = ema.apply([batch_mean, batch_var])
-            ema_mean, ema_var = ema.average(batch_mean), ema.average(batch_var)
-            mean, var = tf.cond(pred=is_train, true_fn=update, false_fn=lambda: (ema_mean, ema_var) )
-            normed = tf.nn.batch_norm_with_global_normalization(bottom, mean, var, beta, gamma, epsilon, False)
-        
-        return normed
-
     def channel_wise_fc_layer(self, input, name): # bottom: (7x7x512)
         _, width, height, n_feat_map = input.get_shape().as_list()
         input_reshape = tf.reshape( input, [-1, width*height, n_feat_map] )
         input_transpose = tf.transpose( a=input_reshape, perm=[2,0,1] )
-
         with tf.compat.v1.variable_scope(name):
             # (512,49,49)
             W = tf.compat.v1.get_variable("W", shape=[n_feat_map,width*height, width*height], initializer=tf.compat.v1.random_normal_initializer(0., 0.005))
@@ -84,8 +60,6 @@ class Reconstruct():
 
         return output_reshape
     
-  
-
     def generator( self, images, is_train ):  
         with tf.compat.v1.variable_scope('GEN'):
             # Encoder part(convolution, fraction-strided convolution, ELU):
@@ -139,19 +113,43 @@ class Reconstruct():
            
             recon = self.conv_2d_transpose( skip1, [3,3,3,channels1],  images.get_shape().as_list(), stride=2, name="recon") 
         return recon
-    # Discriminator is used only while training the model.
-    def discriminator(self, images, is_train, reuse=None):
-        with tf.compat.v1.variable_scope('DIS', reuse=reuse):
-            conv1 = self.conv_2d_layer(images, [4,4,3,64], stride=2, name="conv1" )
-            bn1 = self.leaky_relu(self.batchnorm(conv1, is_train, name='bn1'))
-            conv2 = self.conv_2d_layer(bn1, [4,4,64,128], stride=2, name="conv2")
-            bn2 = self.leaky_relu(self.batchnorm(conv2, is_train, name='bn2'))
-            conv3 = self.conv_2d_layer(bn2, [4,4,128,256], stride=2, name="conv3")
-            bn3 = self.leaky_relu(self.batchnorm(conv3, is_train, name='bn3'))
-            conv4 = self.conv_2d_layer(bn3, [4,4,256,512], stride=2, name="conv4")
-            bn4 = self.leaky_relu(self.batchnorm(conv4, is_train, name='bn4'))
-            conv5 = self.conv_2d_layer(bn3, [4,4,256,512], stride=2, name="conv4")
-            bn5 = self.leaky_relu(self.batchnorm(conv4, is_train, name='bn4'))
-            output = self.fc_layer( bn4, output_size=1, name='output')
+    
+    # THE FOLLOWING CODE IS USED ONLY DURING THE TRAINING PHASE 
+    
+#     def batch_norm(self, bottom, is_train, epsilon=1e-8, name=None):
+#         bottom = tf.clip_by_value( bottom, -100., 100.)
+#         depth = bottom.get_shape().as_list()[-1]
 
-        return output[:,0]       
+#         with tf.compat.v1.variable_scope(name):
+#             gamma = tf.compat.v1.get_variable("gamma", [depth], initializer=tf.compat.v1.constant_initializer(1.))
+#             beta  = tf.compat.v1.get_variable("beta" , [depth], initializer=tf.compat.v1.constant_initializer(0.))
+
+#             batch_mean, batch_var = tf.nn.moments(x=bottom, axes=[0,1,2], name='moments')
+#             ema = tf.train.ExponentialMovingAverage(decay=0.5)
+
+#             def update():
+#                 with tf.control_dependencies([ema_apply_op]):
+#                     return tf.identity(batch_mean), tf.identity(batch_var)
+
+#             ema_apply_op = ema.apply([batch_mean, batch_var])
+#             ema_mean, ema_var = ema.average(batch_mean), ema.average(batch_var)
+#             mean, var = tf.cond(pred=is_train, true_fn=update, false_fn=lambda: (ema_mean, ema_var) )
+#             normed = tf.nn.batch_norm_with_global_normalization(bottom, mean, var, beta, gamma, epsilon, False)
+        
+#         return normed
+
+#     def discriminator(self, images, is_train, reuse=None):
+#         with tf.compat.v1.variable_scope('DIS', reuse=reuse):
+#             conv1 = self.conv_2d_layer(images, [4,4,3,64], stride=2, name="conv1" )
+#             bn1 = self.leaky_relu(self.batch_norm(conv1, is_train, name='bn1'))
+#             conv2 = self.conv_2d_layer(bn1, [4,4,64,128], stride=2, name="conv2")
+#             bn2 = self.leaky_relu(self.batch_norm(conv2, is_train, name='bn2'))
+#             conv3 = self.conv_2d_layer(bn2, [4,4,128,256], stride=2, name="conv3")
+#             bn3 = self.leaky_relu(self.batch_norm(conv3, is_train, name='bn3'))
+#             conv4 = self.conv_2d_layer(bn3, [4,4,256,512], stride=2, name="conv4")
+#             bn4 = self.leaky_relu(self.batch_norm(conv4, is_train, name='bn4'))
+#             conv5 = self.conv_2d_layer(bn3, [4,4,256,512], stride=2, name="conv4")
+#             bn5 = self.leaky_relu(self.batch_norm(conv4, is_train, name='bn4'))
+#             output = self.fc_layer( bn4, output_size=1, name='output')
+
+#         return output[:,0]       
